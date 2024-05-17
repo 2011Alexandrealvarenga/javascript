770951181a3aeba7a6b56c8498ef555d290ec4ad
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2024-05-17 16:15:43 -0300

    email valido

--- validar email/email-validation.js
+++ validar email/email-validation.js
@@ -2,11 +2,11 @@ function ValidateEmail(inputText)
 {
   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   if(inputText.value.match(mailformat)){
-    alert("Valid email address!");
+    alert("Email valido");
     document.form1.text1.focus();
     return true;
   }else{
-    alert("You have entered an invalid email address!");
+    alert("Email invalido");
     document.form1.text1.focus();
     return false;
   }
