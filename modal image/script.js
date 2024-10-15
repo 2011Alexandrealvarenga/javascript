60b9aedf05942c4b1f2809ed9558b530965fc2b3
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2024-10-15 16:35:24 -0300

    modal with image

---
+++ modal image/script.js
@@ -0,0 +1,21 @@
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("openModalBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+window.onload = function (event){
+  setTimeout(() => {
+    modal.style.display = "block";
+  }, 1000);
+} 
